5f22d5efc09dfa17b18ba184693c5d94c03bedb4
Brandon Rosenbaum <brandon.rosenbaum@match.com>
2018-11-16 17:57:59 -0600

    Allow new user creation in Firestore DB.

--- app/src/main/java/com/codebaum/lsdgpresentations/MainActivity.kt
+++ app/src/main/java/com/codebaum/lsdgpresentations/MainActivity.kt
@@ -10,11 +10,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.codebaum.lsdgpresentations.data.Presentation
 import com.codebaum.lsdgpresentations.data.PresentationMapper
 import com.codebaum.lsdgpresentations.data.Repository
+import com.codebaum.lsdgpresentations.utils.toast
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -123,8 +128,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
@@ -143,20 +147,42 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         when (requestCode) {
-            REQUEST_CODE_SIGN_IN -> handleSignInResult(resultCode)
+            REQUEST_CODE_SIGN_IN -> {
+                val response = IdpResponse.fromResultIntent(data)
+                val isNewUser = response?.isNewUser ?: false
+                handleSignInResult(resultCode, isNewUser)
+            }
             REQUEST_CODE_VIEW_PROFILE -> handleViewProfileResult(resultCode)
         }
     }
 
-    private fun handleSignInResult(resultCode: Int) {
+    private fun handleSignInResult(resultCode: Int, isNewUser: Boolean) {
         when (resultCode) {
             Activity.RESULT_OK -> {
-                val user = FirebaseAuth.getInstance().currentUser
-                this.user = user
+                user = FirebaseAuth.getInstance().currentUser
+                if (isNewUser) { // use true as temp workaround to force user creation in Firestore DB
+                    createFirestoreUser(user)
+                }
                 updateView()
             }
             else -> finish()
         }
+    }
+
+    private fun createFirestoreUser(user: FirebaseUser?) {
+        if (user == null) {
+            // do nothing because we have no user data
+            return
+        }
+
+        val userPOJO = hashMapOf<String, Any>()
+        val email = user.email ?: ""
+        userPOJO.put("email", email)
+
+        val db = FirebaseFirestore.getInstance()
+        val usersCollection = db.collection("users")
+        val newUserDocument = usersCollection.document(user.uid)
+        newUserDocument.set(userPOJO, SetOptions.merge())
     }
 
     private fun handleViewProfileResult(resultCode: Int) {
